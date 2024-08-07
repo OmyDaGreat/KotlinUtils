d2753e1c1ec78b277082709cb84728332f39478e
Malefic <ogupta4242@gmail.com>
2024-08-07 01:16:56 -0700

    Added Coroutines Slight refactoring

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,8 @@
-plugins {
-    kotlin("jvm") version "2.0.0"
-}
+plugins { kotlin("jvm") version "2.0.0" }
 
 group = "io.github.omydagreat.kotlinutils"
-version = "1.0"
+
+version = "0.1"
 
 repositories {
     mavenCentral()
@@ -12,16 +11,15 @@ repositories {
 
 val kmlogVersion = "1.5.0"
 val slf4jVersion = "2.0.13"
+val coroutinesVersion = "1.9.0-RC"
 
 dependencies {
     testImplementation(kotlin("test"))
     implementation("org.lighthousegames:logging:$kmlogVersion")
     implementation("org.slf4j:slf4j-simple:$slf4jVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
-kotlin {
-    jvmToolchain(21)
-}
+tasks.test { useJUnitPlatform() }
+
+kotlin { jvmToolchain(21) }
