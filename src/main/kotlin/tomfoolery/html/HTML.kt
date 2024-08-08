7210eb53837efa27d5e2abbb575341dece6e058f
Malefic <ogupta4242@gmail.com>
2024-08-08 12:17:47 -0700

    Added some functionality

--- src/main/kotlin/tomfoolery/html/HTML.kt
+++ src/main/kotlin/tomfoolery/html/HTML.kt
@@ -9,34 +9,53 @@ import io.ktor.server.routing.*
 import kotlinx.html.*
 import kotlinx.html.stream.createHTML
 
+var bool = true
+
 fun DIV.center(display: String = "flex", height: String = "100vh", fontSize: String = "16px", fontFamily: String = "Arial, sans-serif") {
     style = "display: $display; justify-content: center; align-items: center; height: $height; font-size: $fontSize; font-family: $fontFamily;"
 }
 
-fun generateHTMLContent(): String {
-    return createHTML().html {
-        body {
-            div {
-                center(fontSize = "20px", fontFamily = "Verdana, sans-serif")
-                a("https://kotlinlang.org") {
-                    target = ATarget.blank
-                    +"Kotlin main site"
+fun generateHTMLContent(boolValue: Boolean): String = createHTML().html {
+    head {
+        script {
+            unsafe {
+                +"""
+                function callKotlinFunction() {
+                    fetch('/runKotlinFunction')
+                        .then(response => response.text())
+                        .then(data => location.reload());
                 }
+                """.trimIndent()
+            }
+        }
+    }
+    body {
+        div {
+            center(fontSize = "20px", fontFamily = "Verdana, sans-serif")
+            a("https://kotlinlang.org") {
+                target = ATarget.blank
+                +"Kotlin main site (bool: $boolValue)"
+            }
+            button {
+                onClick = "callKotlinFunction()"
+                +"Run Kotlin Function"
             }
         }
     }
 }
 
-fun startServer() {
-    embeddedServer(Netty, port = 8080) {
-        routing {
-            get("/") {
-                call.respondText(generateHTMLContent(), ContentType.Text.Html)
-            }
+fun server() = embeddedServer(Netty, port = 8080) {
+    routing {
+        get("/") {
+            call.respondText(generateHTMLContent(bool), ContentType.Text.Html)
         }
-    }.start(wait = true)
+        get("/runKotlinFunction") {
+            bool = !bool
+            call.respondText("Kotlin function executed!", ContentType.Text.Plain)
+        }
+    }
 }
 
 fun main() {
-    startServer()
+    server().start(wait = true)
 }
