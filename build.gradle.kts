9235ead76fc1de9fab745c448db65df457541fd4
Malefic <ogupta4242@gmail.com>
2024-08-30 10:55:57 -0700

    Changed url to reflect

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,7 +41,7 @@ publishing {
     repositories {
         maven {
             name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/OWNER/REPOSITORY")
+            url = uri("https://maven.pkg.github.com/OmyDaGreat/KotlinUtils")
             credentials {
                 username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
                 password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
